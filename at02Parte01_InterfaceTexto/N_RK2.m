74ae8820bf11b87a45eb3d1e52c0e9a77cf3991b
Adevr <alex.cst.reis@gmail.com>
2019-04-09 22:45:09 +0100

    N_RK2 loop e variaveis

--- at02Parte01_InterfaceTexto/N_RK2.m
+++ at02Parte01_InterfaceTexto/N_RK2.m
@@ -1,5 +1,18 @@
 function y = N_RK2(f, a, b, n, y0)
 
-x = a:h:b;
+h    = (b-a)/n;
+t(1) = a;
+y(1) = y0;
 
-n = length(x);
+sExata = dsolve(['Dy=', f],...
+                ['y(',num2str(a),')=',num2str(y0)]);
+
+
+sRK2 = zeros(size(t));
+            
+
+for i = 1:(length(n)-1)
+    
+end
+
+    
