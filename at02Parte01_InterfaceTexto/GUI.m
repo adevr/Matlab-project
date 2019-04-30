c04f276427970e0797947c8e7e86a6767bcbf6d8
Adevr <alex.cst.reis@gmail.com>
2019-04-30 22:17:00 +0100

    final files c/validação de numeros

--- at02Parte01_InterfaceTexto/GUI.m
+++ at02Parte01_InterfaceTexto/GUI.m
@@ -22,16 +22,16 @@ function varargout = GUI(varargin)
 
 % Edit the above text to modify the response to help GUI
 
-% Last Modified by GUIDE v2.5 15-Apr-2019 20:32:11
+% Last Modified by GUIDE v2.5 25-Apr-2019 18:32:38
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
-                   'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @GUI_OpeningFcn, ...
-                   'gui_OutputFcn',  @GUI_OutputFcn, ...
-                   'gui_LayoutFcn',  [] , ...
-                   'gui_Callback',   []);
+    'gui_Singleton',  gui_Singleton, ...
+    'gui_OpeningFcn', @GUI_OpeningFcn, ...
+    'gui_OutputFcn',  @GUI_OutputFcn, ...
+    'gui_LayoutFcn',  [] , ...
+    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
     gui_State.gui_Callback = str2func(varargin{1});
 end
@@ -63,7 +63,7 @@ guidata(hObject, handles);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = GUI_OutputFcn(hObject, eventdata, handles) 
+function varargout = GUI_OutputFcn(hObject, eventdata, handles)
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -72,120 +72,6 @@ function varargout = GUI_OutputFcn(hObject, eventdata, handles)
 % Get default command line output from handles structure
 varargout{1} = handles.output;
 
-
-
-function edit1_Callback(hObject, eventdata, handles)
-% hObject    handle to edit1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit1 as text
-%        str2double(get(hObject,'String')) returns contents of edit1 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit1_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-
-function edit2_Callback(hObject, eventdata, handles)
-% hObject    handle to edit2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit2 as text
-%        str2double(get(hObject,'String')) returns contents of edit2 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit2_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-
-function edit3_Callback(hObject, eventdata, handles)
-% hObject    handle to edit3 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit3 as text
-%        str2double(get(hObject,'String')) returns contents of edit3 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit3_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit3 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-
-function edit5_Callback(hObject, eventdata, handles)
-% hObject    handle to edit5 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit5 as text
-%        str2double(get(hObject,'String')) returns contents of edit5 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit5_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit5 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-
-function edit6_Callback(hObject, eventdata, handles)
-% hObject    handle to edit6 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit6 as text
-%        str2double(get(hObject,'String')) returns contents of edit6 as a double
-
-
-% --- Executes during object creation, after setting all properties.
-function edit6_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit6 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
 
 
 % --- Executes on selection change in popupmenu1.
@@ -218,4 +104,278 @@ function togglebutton1_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of togglebutton1
+
+
+
+
+function eF_Callback(hObject, eventdata, handles)
+% hObject    handle to eF (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of eF as text
+%        str2double(get(hObject,'String')) returns contents of eF as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function eF_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to eF (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on selection change in selectMethod.
+function selectMethod_Callback(hObject, eventdata, handles)
+% hObject    handle to selectMethod (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns selectMethod contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from selectMethod
+
+
+% --- Executes during object creation, after setting all properties.
+function selectMethod_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to selectMethod (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function eA_Callback(hObject, eventdata, handles)
+% hObject    handle to eA (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of eA as text
+%        str2double(get(hObject,'String')) returns contents of eA as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function eA_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to eA (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function eB_Callback(hObject, eventdata, handles)
+% hObject    handle to eB (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of eB as text
+%        str2double(get(hObject,'String')) returns contents of eB as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function eB_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to eB (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function eN_Callback(hObject, eventdata, handles)
+% hObject    handle to eN (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of eN as text
+%        str2double(get(hObject,'String')) returns contents of eN as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function eN_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to eN (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function eY0_Callback(hObject, eventdata, handles)
+% hObject    handle to eY0 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of eY0 as text
+%        str2double(get(hObject,'String')) returns contents of eY0 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function eY0_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to eY0 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in execMethod.
+function execMethod_Callback(hObject, eventdata, handles)
+% hObject    handle to execMethod (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+method = handles.selectMethod.Value;
+strF  =  get(handles.eF,'String');
+strY0 = get(handles.eY0,'String');
+eA = get(handles.eA, 'String');
+eB = get(handles.eB, 'String');
+eN = get(handles.eN, 'String');
+eY0 = get(handles.eY0, 'String');
+
+disp(isnan(str2double(eA)));
+
+if isnan(str2double(eA)) == 1 | isnan(str2double(eB)) == 1 | isnan(str2double(eN)) == 1 | isnan(str2double(eY0)) == 1
+    if(isnan(str2double(eA)))
+        set(handles.eA,'string', 0);
+        errordlg(sprintf('O valor de a passou para 0, pois o seu valor era inválido!\nInserir dados válidos no campo a!'));
+    elseif(isnan(str2double(eB)))
+        set(handles.eB,'string', 0);
+        errordlg(sprintf('O valor de b passou para 0, pois o seu valor era inválido!\nInserir dados válidos no campo b!'));
+    elseif(isnan(str2double(eN)))
+        set(handles.eN,'string', 0);
+        errordlg(sprintf('O valor de n passou para 0, pois o seu valor era inválido!\nInserir dados válidos no campo n!'));
+    elseif(isnan(str2double(eY0)))
+        set(handles.eY0,'string', 0);
+        errordlg(sprintf('O valor de y0 passou para 0, pois o seu valor era inválido!\nInserir dados válidos no campo y0!'));
+    end
     
+elseif str2double(eA) == 0 & str2double(eB)==0 & str2double(eN)==0 & str2double(eY0)==0
+    warndlg(sprintf('Os valores de A,B,N e Y0 estão a 0\nInserir valores inteiros!'));
+else
+    
+    f = @(t,y) eval(vectorize(strF));
+    a = str2double(get(handles.eA, 'String'));
+    b = str2double(get(handles.eB, 'String'));
+    n = str2double(get(handles.eN, 'String'));
+    y0 = str2double(get(handles.eY0, 'String'));
+    
+    yEuler = N_Euler(f,a,b,n,y0); % chamada ao método de Euler
+    yEulerM = N_Euler_v2(f,a,b,n,y0); % chamada ao metodo de Euler Modificado
+    yRK2   = N_RK2(f,a,b,n,y0);% chamada ao método RK2
+    yRK4   = N_RK4(f,a,b,n,y0);% chamada ao método RK4
+    yODE45 =  ODE45 (f,a,b,n,y0); % chamada ao metodo ODE45
+    t = a:(b-a)/n:b; % discretização do domínio
+    
+    sExacta = dsolve(['Dy=' strF],['y(' num2str(t(1)) ')=' strY0]);
+    yExacta = eval(vectorize(char(sExacta)));
+    
+    switch(method)
+        case 2
+            plot(t,yEuler,'-r')
+            hold on
+            plot(t,yExacta,'b')
+            hold off
+            legend('Euler','Solução Exacta')
+            set(handles.uitable2,'ColumnName',{'t','Solução Exacta',...
+                'Euler','Erro Euler'});
+            cTabela = [t.',yExacta.', yEuler.',abs(yEuler-yExacta).'];
+            set(handles.uitable2,'Data',num2cell(cTabela));
+        case 3
+            plot(t,yEulerM,'-r')
+            hold on
+            plot(t,yExacta,'b')
+            hold off
+            legend('Euler Melhorado','Solução Exacta')
+            set(handles.uitable2,'ColumnName',{'t','Solução Exacta',...
+                'Euler Melhorado','Erro Euler'});
+            cTabela = [t.',yExacta.', yEulerM.',abs(yEulerM-yExacta).'];
+            set(handles.uitable2,'Data',num2cell(cTabela));
+        case 4
+            plot(t,yRK2,'-r')
+            hold on
+            plot(t,yExacta,'b')
+            hold off
+            legend('Runge Kutta Ordem 2','Solução Exacta')
+            set(handles.uitable2,'ColumnName',{'t','Solução Exacta',...
+                'RK2','Erro RK2'});
+            cTabela = [t.',yExacta.', yRK2.',abs(yRK2-yExacta).'];
+            set(handles.uitable2,'Data',num2cell(cTabela));
+        case 5
+            plot(t,yRK4,'-r')
+            hold on
+            plot(t,yExacta,'b')
+            hold off
+            legend('Runge Kutta Ordem 4','Solução Exacta')
+            set(handles.uitable2,'ColumnName',{'t','Solução Exacta',...
+                'RK4','Erro RK4'});
+            cTabela = [t.',yExacta.', yRK4.',abs(yRK4-yExacta).'];
+            set(handles.uitable2,'Data',num2cell(cTabela));
+        case 6
+            plot(t,yODE45,'-r')
+            hold on
+            plot(t,yExacta,'b')
+            hold off
+            legend('ODE45','Solução Exacta')
+            set(handles.uitable2,'ColumnName',{'t','Solução Exacta',...
+                'ODE45','Erro ODE45'});
+            cTabela = [t.',yExacta.', yODE45.',abs(yODE45-yExacta).'];
+            set(handles.uitable2,'Data',num2cell(cTabela));
+    end
+    
+end
+
+% --- Executes on button press in csvExport.
+function csvExport_Callback(hObject, eventdata, handles)
+% hObject    handle to csvExport (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+method = handles.selectMethod.Value;
+switch(method)
+    case 2
+        writecell(get(handles.uitable2, 'Data'),'/home/adevreis/Documents/Matlab-project/at02Parte01_InterfaceTexto/table.csv');
+
+    case 3
+        headers = {'T', 'Solução Exata', 'Euler Melhorado', 'Erro Euler Melhorado'};
+        csvwrite('/home/adevreis/Documents/Matlab-project/at02Parte01_InterfaceTexto/table.csv', get(handles.uitable2, 'Data'), headers);
+
+    case 4
+        headers = {'T', 'Solução Exata', 'RK2', 'Erro RK2'};
+        csvwrite('/home/adevreis/Documents/Matlab-project/at02Parte01_InterfaceTexto/table.csv', get(handles.uitable2, 'Data'), headers);
+
+    case 5
+        headers = {'T', 'Solução Exata', 'RK4', 'Erro RK4'};
+        csvwrite('/home/adevreis/Documents/Matlab-project/at02Parte01_InterfaceTexto/table.csv', get(handles.uitable2, 'Data'), headers);
+
+end
+
+% --- Executes on button press in xlsxExtract.
+function xlsxExtract_Callback(hObject, eventdata, handles)
+% hObject    handle to xlsxExtract (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
--- at02Parte01_InterfaceTexto/N_RK4.m
+++ at02Parte01_InterfaceTexto/N_RK4.m
@@ -1,3 +1,16 @@
+%RK4  Método de Runge-kutta de ordem 4 para PVI.
+%   y = RK4(f,a,b,n,y0) Método numérico para a resolução de um PVI
+%   y'= f(t,y) com t=[a, b] e y(a)=y0 condição inicial  
+%
+%INPUT:
+%   f - função do 2ª membro da Equação Diferencial
+%   [a, b] - extremos do intervalo da variável independente t
+%   n - número de subintervalos ou iterações do método
+%   y0 - condição inicial t=a -> y=y0
+%OUTPUT: 
+%   y - vector das aproximações discretas da solução exacta
+%   y(i+1) = y(i)+h*f(t(i),y(i)) , i =0,1,...,n-1
+
 function y = N_RK4(f, a, b, n, y0)
 
     h = (b-a)/n;
